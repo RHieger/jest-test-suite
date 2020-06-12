b1711ef4ef39944912fd7098e714bc36b519b02e
Robert Hieger <rhieger@gmail.com>
2020-06-12 19:54:20 -0400

    Create first two matcher tests

---
+++ app.js
@@ -0,0 +1,26 @@
+const greeting = () => {
+  let message = "Hello, World!";
+  return message;
+};
+
+const groceries = [
+  "1 dozen eggs",
+  "milk",
+  "2 boxes golden raisins",
+  "2 containers whipped butter",
+  "oatmeal",
+  "chick pea paella",
+  "sparkling ice drinks",
+  "fruit juice",
+  "2 lbs. seedless grapes",
+  "2 lb. navel oranges",
+  "10 bananas",
+  "1 bunch broccoli",
+  "2 boxes no sugar added crunchy Klondike bars",
+  "2 cannisters ground French Vanilla Coffee"
+];
+
+module.exports = {
+  greeting,
+  groceries
+};
--- app.test.js
+++ app.test.js
@@ -1,5 +1,13 @@
+const { greeting, groceries } = require("./app");
+
 describe("toBe(value)", () => {
   test("returned message is Hello, World", () => {
-    expect(greeting().toBe("Hello, World!"));
+    expect(greeting()).toBe("Hello, World!");
+  });
+});
+
+describe("toContain(element)", () => {
+  test("the grocery list contains oatmeal", () => {
+    expect(groceries).toContain("oatmeal");
   });
 });
